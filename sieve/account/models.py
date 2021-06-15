aa9a9ac45490880dc11e5543959455d10df7dd49
ShinsSeungyeol <85426685+ShinsSeungyeol@users.noreply.github.com>
2021-06-15 17:33:46 +0900

    add user model, modify the settings.file for static load

--- sieve/account/models.py
+++ sieve/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model) : 
+    email = models.EmailField(max_length=254, unique='True', verbose_name='사용자 이메일')
+    name =  models.CharField(max_length=20, blank=True, verbose_name='사용자 이름')
+    password = models.CharField(max_length=20, verbose_name='비밀 번호')
+    phone_number = models.CharField(max_length=11, verbose_name='핸드폰 번호')
+
+    def __str__(self) :
+        return self.email 
--- sieve/account/views.py
+++ sieve/account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import SignupForm  
 
 # Create your views here.
+def signup(request) :
+    signup_form = SignupForm()
+    return render(request, 'account/signup.html', {'signup_form' : signup_form})
+    
