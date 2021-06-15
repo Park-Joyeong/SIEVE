3f3f83826891c53e4f3279e7d0edc3322dd3a2f6
ShinsSeungyeol <85426685+ShinsSeungyeol@users.noreply.github.com>
2021-06-16 00:16:19 +0900

    add validator of phone_number, password, name

--- sieve/account/models.py
+++ sieve/account/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from . import validators
 # Create your models here.
 
 class User(models.Model) : 
-    email = models.EmailField(max_length=254, unique='True', verbose_name='사용자 이메일')
-    name =  models.CharField(max_length=20, blank=True, verbose_name='사용자 이름')
-    password = models.CharField(max_length=20, verbose_name='비밀 번호')
-    phone_number = models.CharField(max_length=11, verbose_name='핸드폰 번호')
+    email = models.EmailField(max_length=100, unique=True, verbose_name='이메일')
+    name =  models.CharField(max_length=20, blank=True, validators=[validators.validate_name], verbose_name='이름')
+    password = models.CharField(max_length=20, validators=[validators.validate_password], verbose_name='비밀 번호')
+    phone_number = models.CharField(max_length=13, validators=[validators.validate_phone_number], verbose_name='핸드폰 번호')
 
     def __str__(self) :
         return self.email 
--- sieve/account/views.py
+++ sieve/account/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
-from .forms import SignupForm  
+from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
+from .models import User
+
 
 # Create your views here.
 def signup(request) :
-    signup_form = SignupForm()
-    return render(request, 'account/signup.html', {'signup_form' : signup_form})
+    if request.method == 'GET' :
+        return render(request, 'account/signup.html')
+    elif request.method == 'POST' :
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        name = request.POST.get("name")
+        phone_number = request.POST.get("phone_number")
+        
+        res_data = {}
+        res_data['is_success'] = True
+
+        new_user = User(email=email, password=password, name=name, phone_number=phone_number)
+        try :
+            new_user.full_clean()
+        except ValidationError as error :
+            error_message = ''
+
+            if 'email' in error.message_dict :
+                error_message = error.message_dict['email'][0]
+            elif 'password' in error.message_dict :
+                error_message = error.message_dict['password'][0]
+            elif 'name' in error.message_dict :
+                error_message = error.message_dict['name'][0]
+            else :
+                error_message = error.message_dict[phone_number][0]
+
+            res_data[ 'error_message'] = error_message
+            res_data['is_success'] = False
+            return render(request, 'account/signup.html', {'res_data' : res_data})
+
+        new_user.save() 
+        return render(request, 'account/signup.html', {'res_data' : res_data})
+        #후에 종목 편집 페이지로 리다이렉트 시켜야함!
+
     
