63e18a2de498ca71265d613c1552334bf87d6bf5
btiac1129 <btiac1129@gmail.com>
2021-06-17 11:43:49 +0900

    중간 커밋

--- sieve/account/views.py
+++ sieve/account/views.py
@@ -5,74 +5,76 @@ from .models import User
 
 
 # Create your views here.
-def signup(request) :
-    if request.method == 'POST' :
+def signup(request):
+    if request.method == 'POST':
         email = request.POST.get("email")
         password = request.POST.get("password")
         name = request.POST.get("name")
         phone_number = request.POST.get("phone_number")
         res_data = {}
-        
-        new_user = User(email=email, password=password, name=name, phone_number=phone_number)
-        try :
+
+        new_user = User(email=email, password=password,
+                        name=name, phone_number=phone_number)
+        try:
             new_user.full_clean()
-        except ValidationError as error :
+        except ValidationError as error:
             error_message = ''
 
-            if 'email' in error.message_dict :
+            if 'email' in error.message_dict:
                 error_message = error.message_dict['email'][0]
-            elif 'password' in error.message_dict :
+            elif 'password' in error.message_dict:
                 error_message = error.message_dict['password'][0]
-            elif 'name' in error.message_dict :
+            elif 'name' in error.message_dict:
                 error_message = error.message_dict['name'][0]
-            else :
+            else:
                 error_message = error.message_dict['phone_number'][0]
 
-            res_data[ 'error_message'] = error_message
+            res_data['error_message'] = error_message
             res_data['is_success'] = False
-            return render(request, 'account/signup.html', {'res_data' : res_data})
+            return render(request, 'account/signup.html', {'res_data': res_data})
 
-        new_user.save() 
+        new_user.save()
         request.session['user_name'] = name
         res_data['is_success'] = True
-        return render(request, 'account/signup.html', {'res_data' : res_data})
-        #후에 종목 편집 페이지로 리다이렉트 시켜야함!
+        return render(request, 'account/signup.html', {'res_data': res_data})
+        # 후에 종목 편집 페이지로 리다이렉트 시켜야함!
 
-    elif request.method == 'GET' :
+    elif request.method == 'GET':
         return render(request, 'account/signup.html')
 
-        
-def check_mail(request) :
-    if request.method == 'GET' :
+
+def check_mail(request):
+    if request.method == 'GET':
         print(request)
         new_email = request.GET.get("email")
-        
-        try :
-            user = User.objects.get(email = new_email) 
-        except User.DoesNotExist: 
-            can_use_this_email = True
-            return JsonResponse({'can_use_this_email' : can_use_this_email})
-        can_use_this_email = False
-        return JsonResponse({'can_use_this_email' : can_use_this_email})
-        
 
-def signin(request): 
+        try:
+            user = User.objects.get(email=new_email)
+        except User.DoesNotExist:
+            can_use_this_email = True
+            return JsonResponse({'can_use_this_email': can_use_this_email})
+        can_use_this_email = False
+        return JsonResponse({'can_use_this_email': can_use_this_email})
+
+
+def signin(request):
     if request.method == 'POST':
+        print(request.POST)
         email = request.POST.get('email')
         password = request.POST.get('password')
+        print(email, password)
         res_data = {}
-        if not (email and password) :
+        if not (email and password):
             res_data['error'] = '이메일과 비밀번호 모두 입력해주세요.'
-            return render(request, 'account/signin.html', {'res_data' : res_data})
-        else :
+            return render(request, 'account/signin.html', {'res_data': res_data})
+        else:
             user = User.objects.get(email=email)
             if user.password == password:
                 request.session['user'] = email
                 return redirect('')
-                 #후에 종목 편집 페이지로 리다이렉트
-            else :
+                # 후에 종목 편집 페이지로 리다이렉트
+            else:
                 res_data['error'] = '이메일 또는 비밀번호가 틀립니다.'
-                return render(request, 'account/signin.html', {'res_data' : res_data})
-    else : 
+                return render(request, 'account/signin.html', {'res_data': res_data})
+    else:
         return render(request, 'account/signin.html')
-    #return render(request, 'account/signin.html', {'res_data' : res_data})     
--- sieve/sieve/static/account/js/signin.js
+++ sieve/sieve/static/account/js/signin.js
@@ -1,26 +1,38 @@
 const $email = document.querySelector("#email");
+const $password = document.querySelector("#password");
 const $emailError = document.querySelector("#email-error");
 const $signin = document.querySelector("#signin-form");
 
-$signin.onsubmit = async(e) => {
+$signin.onclick = async(e) => {
     e.preventDefault();
 
-    let url = "./signin/checkemail";
-    let data = {
-        email: $email.value,
-    };
+    let url = "./signin";
+    let formData = new FormData();
+    formData.append("email", $email);
+    formData.append("password", $password);
     let response = await fetch(url, {
         method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-        },
+        body: formData,
     });
-    console.log(response);
-    let result = await response.json();
 
-    if (result["can_use_this_email"]) {
-        $emailError.text("가입되지 않은 이메일입니다.");
-        $emailError.show();
-    }
+    let result = await response.json();
+    console.log("Success: ", result);
+    // let url = "./signin/checkemail";
+    // let data = {
+    //     email: $email.value,
+    // };
+    // let response = await fetch(url, {
+    //     method: "POST",
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    // });
+
+    // let result = await response.json();
+
+    // // if (result["can_use_this_email"]) {
+    // //     $emailError.text("가입되지 않은 이메일입니다.");
+    // //     $emailError.show();
+    // // }
 };
