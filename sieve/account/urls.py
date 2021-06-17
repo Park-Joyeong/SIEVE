ccd59dcdabad002cc770d9acbecc1f55468a4654
btiac1129 <btiac1129@gmail.com>
2021-06-18 01:21:58 +0900

    FIX: signin 기능 완료. 추후 애니메이션 추가 예정

--- sieve/account/urls.py
+++ sieve/account/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signup/checkemail', views.check_mail, name='check_email'),
     path('signin', views.signin, name='signin'),
-    path('signin/checkemail', views.check_mail, name='check_email')
-    # path('signin/checkpassword')
 ]
--- sieve/sieve/static/account/js/signin.js
+++ sieve/sieve/static/account/js/signin.js
@@ -5,10 +5,42 @@ const $csrfmiddlewaretoken = document.querySelector(
 );
 const $emailError = document.querySelector("#email-error");
 const $passwordError = document.querySelector("#password-error");
+const $signinInput = document.querySelector(".signin-input");
 const $signin = document.querySelector("#signin-form");
 const $signinBtn = document.querySelector("#signin-btn");
 
+const isEmail = (email) => {
+    const email_regex =
+        /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    if (!email_regex.test(email)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
 const executeSignIn = async() => {
+    // isEmail
+    if (!$email.value) {
+        $emailError.innerText = "이메일을 입력하세요.";
+        $emailError.classList.remove("d-none");
+        $email.focus();
+        return;
+    } else if (!isEmail($email.value)) {
+        $emailError.innerText = "이메일 형식이 아닙니다.";
+        $emailError.classList.remove("d-none");
+        $email.focus();
+        return;
+    }
+
+    // check password
+    if (!$password.value) {
+        $passwordError.innerText = "비밀번호를 입력하세요.";
+        $passwordError.classList.remove("d-none");
+        $password.focus();
+        return;
+    }
+
     // create formData for fetch
     let url = "./signin";
     let formData = new FormData();
@@ -27,16 +59,23 @@ const executeSignIn = async() => {
     return result;
 };
 
+$email.onkeyup = (e) => {
+    // animate 예정
+    $emailError.classList.add("d-none");
+};
+
+$password.onkeyup = (e) => {
+    // animate 예정
+    $passwordError.classList.add("d-none");
+};
+
 $signinBtn.onclick = async(e) => {
     e.preventDefault();
 
-    // isEmailExist();
-
-    executeSignIn().then((data) => {
-        if (data["is_success"]) {
-            window.location.href = data["url_to_redirect"];
-        } else {
-            alert(data["error_message"]);
-        }
-    });
+    const data = await executeSignIn();
+    if (data["is_success"]) {
+        window.location.href = data["url_to_redirect"];
+    } else {
+        alert(data["error_message"]);
+    }
 };
