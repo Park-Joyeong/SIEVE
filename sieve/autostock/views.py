5ba33f86b760f707e56c974a9f50747f79192a66
sk-moon <star3qp@naver.com>
2021-06-17 19:28:26 +0900

    views.py 수정 - GET으로 요청하는 경우 처리

--- sieve/autostock/views.py
+++ sieve/autostock/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+from .models import DailyTradingInfo, ListedCompany, StocksOfInterest
 
 def edit_interest(request):
-    return render(request, 'autostock/interest_edit.html')
+    if request.method == 'GET' :
+        # qs : Query Set
+        qs_listed_company = ListedCompany.objects.all()
+        
+        qs_stocks_of_interest = StocksOfInterest.objects.all()
+        qs_stocks_of_interest = qs_stocks_of_interest.filter(user_id=request.session['user_id'])
+
+        render_data = {
+            'listed_company' : serializers.serialize("json", qs_listed_company),
+            'stocks_of_interest' : serializers.serialize("json", qs_stocks_of_interest)
+        }
+        return render(request, 'autostock/interest_edit.html', render_data)
