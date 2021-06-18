5b86eeb57039a2abb543fd6e92486f81568b720b
Park_Joyeong <drhamst@gmail.com>
2021-06-18 10:36:15 +0900

    commit

--- sieve/sieve/static/autostock/js/autostock.js
+++ sieve/sieve/static/autostock/js/autostock.js
@@ -1,6 +1,10 @@
+const $csrfmiddlewaretoken = document.querySelector(
+  "input[name=csrfmiddlewaretoken]"
+);
+
 function componentRendering(obj) {
-  renderListedCompany(obj['listedCompany']);
-  renderStocksOfInterest(obj['stocksOfInterest']);
+  renderListedCompany(obj["listedCompany"]);
+  renderStocksOfInterest(obj["stocksOfInterest"]);
 }
 
 function renderListedCompany(listedCompany) {
@@ -9,4 +13,25 @@ function renderListedCompany(listedCompany) {
 
 function renderStocksOfInterest(stocksOfInterest) {
   console.log(stocksOfInterest);
+}
+
+function test(num) {
+  let formData = new FormData();
+  formData.append("csrfmiddlewaretoken", $csrfmiddlewaretoken.value);
+  if(num===0) {
+    formData.append("selected", []);
+  }
+  if(num===1) {
+    formData.append("selected", ['001122']);
+  }
+  if(num===2) {
+    formData.append("selected", ['001122', '068270']);
+  }
+  
+  fetch("", {
+    method: "post",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {});
 }
