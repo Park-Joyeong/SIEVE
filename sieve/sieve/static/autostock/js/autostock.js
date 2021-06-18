58ac94053c3a33367521bf035c99835d03c48c2b
Park_Joyeong <drhamst@gmail.com>
2021-06-18 09:05:43 +0900

    commit

---
+++ sieve/sieve/static/autostock/js/autostock.js
@@ -0,0 +1,12 @@
+function componentRendering(obj) {
+  renderListedCompany(obj['listedCompany']);
+  renderStocksOfInterest(obj['stocksOfInterest']);
+}
+
+function renderListedCompany(listedCompany) {
+  console.log(listedCompany);
+}
+
+function renderStocksOfInterest(stocksOfInterest) {
+  console.log(stocksOfInterest);
+}
