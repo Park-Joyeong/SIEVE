d4ccdf2f692c1d02319a6df27b01835505ee1edc
Park_Joyeong <drhamst@gmail.com>
2021-06-19 12:32:16 +0900

    commit

--- sieve/sieve/static/autostock/js/interest_edit.js
+++ sieve/sieve/static/autostock/js/interest_edit.js
@@ -2,6 +2,8 @@ const $csrfmiddlewaretoken = document.querySelector(
   "input[name=csrfmiddlewaretoken]"
 );
 
+const $itemListedCompany = document.querySelector(".item-listed-company");
+
 function componentRendering(obj) {
   renderListedCompany(obj["listedCompany"]);
   renderStocksOfInterest(obj["stocksOfInterest"]);
@@ -9,6 +11,17 @@ function componentRendering(obj) {
 
 function renderListedCompany(listedCompany) {
   console.log(listedCompany);
+  var parent = document.querySelector("#contents-listed-company");
+  listedCompany.forEach(function () {
+    var ele = $itemListedCompany.cloneNode(true);
+
+    console.log("origin");
+    console.log(parent);
+    // console.log('next');
+    // console.log(origin.children);
+
+    parent.append(ele);
+  });
 }
 
 function renderStocksOfInterest(stocksOfInterest) {
@@ -18,16 +31,16 @@ function renderStocksOfInterest(stocksOfInterest) {
 function test(num) {
   let formData = new FormData();
   formData.append("csrfmiddlewaretoken", $csrfmiddlewaretoken.value);
-  if(num===0) {
+  if (num === 0) {
     formData.append("selected", []);
   }
-  if(num===1) {
-    formData.append("selected", ['001122']);
+  if (num === 1) {
+    formData.append("selected", ["001122"]);
   }
-  if(num===2) {
-    formData.append("selected", ['001122', '068270']);
+  if (num === 2) {
+    formData.append("selected", ["001122", "068270"]);
   }
-  
+
   fetch("", {
     method: "post",
     body: formData,
