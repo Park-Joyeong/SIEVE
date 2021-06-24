1d3568a2cefc5fc43063443e803e7ffde39f555a
Park_Joyeong <drhamst@gmail.com>
2021-06-24 22:08:47 +0900

    Dounut temporary complete

--- sieve/sieve/static/autostock/js/donut_chart.js
+++ sieve/sieve/static/autostock/js/donut_chart.js
@@ -1,18 +1,20 @@
 const data = {
   labels: [
-    'Red',
-    'Blue',
-    'Yellow'
+    '삼성전자',
+    'SK하이닉스',
+    'NAVER',
+    '셀트리온',
+    '카카오'
   ],
   datasets: [{
     label: 'My First Dataset',
-    data: [300, 50, 100],
+    data: [300, 50, 100, 40, 60],
     backgroundColor: [
       'rgb(255, 99, 132)',
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
     ],
-    hoverOffset: 4
+    hoverOffset: 0
   }]
 };
 
@@ -20,7 +22,23 @@ const data = {
 const config = {
   type: 'doughnut',
   data: data,
+  options: {
+    responsive: true,
+    legend: {
+      position: 'right',
+    },
+    plugins: {
+      
+      datalabels: {
+        formatter: (value) => {
+          return value + '%';
+        }
+      }
+    }
+  },
 };
+
+
 
 var myChart = new Chart(
   document.getElementById('myChart'),
