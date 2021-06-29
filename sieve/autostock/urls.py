ecac1449d70fe9c02ce40ca5d3cea5db57344b54
leemalgum <lmu1289@gmail.com>
2021-06-29 22:44:12 +0900

    경로 추가했어요

--- sieve/autostock/urls.py
+++ sieve/autostock/urls.py
@@ -7,5 +7,6 @@ app_name = 'autostock'
 urlpatterns = [
 
     path('interest/edit/', views.edit_interest, name="edit_interest"),
-    path('dashboard/show/', views.show_dashboard, name="show_dashboard")
+    path('dashboard/show/', views.show_dashboard, name="show_dashboard"),
+    path('dashboard/show/', views.selected_stock, name="selected_stock")
 ]
