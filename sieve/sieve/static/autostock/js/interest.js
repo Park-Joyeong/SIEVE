752f55bdd983611af7c6d5fd672289e0820e8304
Park_Joyeong <drhamst@gmail.com>
2021-06-30 11:17:35 +0900

    관심종목부분 데이터 받아서 보여주기 완성

--- sieve/sieve/static/autostock/js/interest.js
+++ sieve/sieve/static/autostock/js/interest.js
@@ -1,72 +1,40 @@
-const $listItem = document.querySelector(".list-item");
+const $itemInterestList = document.querySelector(".item-interest-list");
 
 window.onload = (event) => {
-  renderStocksOfInterest([
-    {
-      fields:{
-        company_code:123456,
-        company_name:'삼성전자',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    },{
-      fields:{
-        company_code:123456,
-        company_name:'엘지전자',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    },{
-      fields:{
-        company_code:123456,
-        company_name:'삼성물산',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    },{
-      fields:{
-        company_code:123456,
-        company_name:'오뚜기',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    },{
-      fields:{
-        company_code:123456,
-        company_name:'오뚜기',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    },{
-      fields:{
-        company_code:123456,
-        company_name:'오뚜기',
-        updated:'2020-12-12',
-        category:'전자업',
-      }
-    }
-  ])
+
+
+  fetch("../../interest/json")
+    .then((response) => response.json())
+    .then((data) => {
+      rtnArr = data['data'];
+      renderStocksOfInterest(rtnArr)
+    });
+
 };
 
 function renderStocksOfInterest(stocksOfInterest) {
   var parent = document.querySelector("#contents-interest-company");
 
-  stocksOfInterest.forEach(function (obj) {
-    var item = $listItem.cloneNode(true);
+  stocksOfInterest.forEach( obj => {
+    var item = $itemInterestList.cloneNode(true);
     var spanCompanyName = item.getElementsByClassName("company-name");
     var spanCode = item.getElementsByClassName("code");
     var spanUpdated = item.getElementsByClassName("updated");
     var spanCategory = item.getElementsByClassName("category");
 
     // Find companyname, updated, category
-    var code = obj.fields.company_code;
-    var company_name = obj.fields.company_name;
-    var updated = obj.fields.updated;
-    var category = obj.fields.category;
+    var code = obj.company_code;
+    var company_name = obj.company_name;
+    var updated = obj.created;
+    var category = obj.category;
     spanCompanyName[0].innerHTML = company_name;
     spanCode[0].innerHTML = code;
     spanUpdated[0].innerHTML = updated;
     spanCategory[0].innerHTML = category;
     parent.append(item);
   });
+}
+
+function goToEditPage() {
+  window.location.href = "/interest/edit";
 }
