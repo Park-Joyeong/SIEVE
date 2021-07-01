b222993d5c43d7aa7e348cc382a73bb68844b64f
Park_Joyeong <drhamst@gmail.com>
2021-07-01 15:13:15 +0900

    sync from main_for_be

--- sieve/autostock/models.py
+++ sieve/autostock/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class DailyTradingInfo(models.Model):
     id = models.BigAutoField(primary_key=True)
-    company_code = models.ForeignKey('ListedCompany', models.DO_NOTHING, db_column='company_code', blank=True, null=True)
+    company_code = models.ForeignKey('ListedCompany', models.CASCADE, db_column='company_code', blank=True, null=True)
     date = models.DateField(blank=True, null=True)
     open = models.BigIntegerField(blank=True, null=True)
     high = models.BigIntegerField(blank=True, null=True)
@@ -27,10 +27,27 @@ class ListedCompany(models.Model):
 
 
 class StocksOfInterest(models.Model):
-    user_id = models.ForeignKey('account.User', models.DO_NOTHING, db_column='user_id', blank=True, null=True)
-    company_code = models.ForeignKey('ListedCompany', models.DO_NOTHING, db_column='company_code', blank=True, null=True)
+    user_id = models.ForeignKey('account.User', models.CASCADE, db_column='user_id', blank=True, null=True)
+    company_code = models.ForeignKey('ListedCompany', models.CASCADE, db_column='company_code', blank=True, null=True)
     created = models.DateField(blank=True, null=True)
 
-
-
 # Create your models here.
+# branch 만듦
+# 주식잔고, 실시간 계좌 잔고 테이블 생성
+
+class StockBalance(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey('account.User', on_delete=models.CASCADE, db_column='user_id', verbose_name='유저아이디')
+    company_code = models.ForeignKey('ListedCompany', on_delete=models.CASCADE, db_column='company_code', verbose_name='종목코드')
+    holding_quantity = models.BigIntegerField(verbose_name='보유수량')
+    purchase_unit_price = models.BigIntegerField(verbose_name='매입단가')
+
+
+
+class RealtimeAccountBalance(models.Model):
+    # id당 계좌 번호 1개
+    account_number = models.CharField(primary_key=True, max_length=30, verbose_name='계좌번호')
+    user_id = models.ForeignKey('account.User', on_delete=models.CASCADE, db_column='user_id', verbose_name='유저아이디')
+    total_evaluation_amount = models.BigIntegerField(verbose_name='총평가금액')
+    total_valuation_profit_or_loss = models.BigIntegerField(verbose_name='총평가손익')
+    rate_of_return = models.BigIntegerField(verbose_name='총손익률')
