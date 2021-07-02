7e2b093bd65755d21073a42fe9835febb281ed11
Park_Joyeong <drhamst@gmail.com>
2021-07-02 22:49:16 +0900

    CandleChart Complete

---
+++ sieve/sieve/static/autostock/js/candlestick_chart.js
@@ -0,0 +1,80 @@
+const candleStick = {
+
+
+  /*
+  Args:
+    obj = {companyCode:'000020', companyName:'동화약품'}
+  */
+  loadCandlestick: function (obj) {
+    const { companyCode, companyName } = obj;
+
+    let resultArr = [];
+    fetch("../getTradingInfo?" + "companyCode=" + companyCode)
+      .then((response) => response.json())
+      .then((data) => {
+        var tradingInfo = JSON.parse(data.tradingInfo);
+
+        var tempArr = [];
+
+        tradingInfo.forEach(element => {
+          tempArr = [];
+          tempArr.push(new Date(element.fields.date + 'T00:00:00').getTime());
+          tempArr.push(element.fields.open);
+          tempArr.push(element.fields.high);
+          tempArr.push(element.fields.low);
+          tempArr.push(element.fields.close);
+          resultArr.push(tempArr);
+        });//foreach
+
+        
+        resultArr.sort(function(a, b){return a[0] - b[0]});
+
+        const chart = Highcharts.stockChart("container", {
+          rangeSelector: {
+            selected: 1,
+          },
+
+          title: {
+            text: companyName + "(" + companyCode + ")"
+          },
+
+          series: [{
+            type: "candlestick",
+            name: companyName + "(" + companyCode + ")",
+            data: resultArr,
+            dataGrouping: {
+              units: [
+                [
+                  "week", // unit name
+                  [1], // allowed multiples
+                ],
+                ["month", [1, 2, 3, 4, 6]],
+              ],
+            },
+          },],
+          responsive: {
+            rules: [{
+              condition: {
+                maxWidth: 500,
+              },
+              chartOptions: {
+                chart: {
+                  height: 205,
+                },
+                subtitle: {
+                  text: null,
+                },
+                navigator: {
+                  enabled: false,
+                },
+              },
+            },],
+          },
+        });
+
+        chart.setSize(null);
+
+      });//callback
+
+  }//loadCandlestick()
+};//const candlestick
--- sieve/sieve/static/autostock/js/interest.js
+++ sieve/sieve/static/autostock/js/interest.js
@@ -8,6 +8,7 @@ window.onload = (event) => {
     .then((data) => {
       rtnArr = data['data'];
       renderStocksOfInterest(rtnArr)
+      candleStick.loadCandlestick({companyCode:rtnArr[0]['company_code'], companyName:rtnArr[0]['company_name']});
     });
 
 };
@@ -40,79 +41,12 @@ function goToEditPage() {
 }
 
 function showCandleChart(dom) {
+  obj = {};
   clickedCompanyCode = dom.getElementsByClassName("code")[0].innerHTML;
   clickedCompanyName = dom.getElementsByClassName("company-name")[0].innerHTML;
-  let resultArr = [];
-  fetch("../getTradingInfo?" + "companyCode=" + clickedCompanyCode)
-    .then((response) => response.json())
-    .then((data) => {
-      var tradingInfo = JSON.parse(data.tradingInfo);
-      var tempArr = [];
+  obj['companyCode'] = clickedCompanyCode;
+  obj['companyName'] = clickedCompanyName;
 
-      tradingInfo.forEach(element => {
-        tempArr = [];
-        tempArr.push(new Date(element.fields.date+'T00:00:00').getTime());
-        tempArr.push(element.fields.open);
-        tempArr.push(element.fields.high);
-        tempArr.push(element.fields.low);
-        tempArr.push(element.fields.close);
+  candleStick.loadCandlestick(obj);
 
-
-
-
-
-        //TODO
-        resultArr.push(JSON.parse(JSON.stringify(tempArr)));//Deep Copy
-      });
-      console.log('aaa')
-      console.log(resultArr)
-    });
-    console.log('bbb')
-    console.log(resultArr)
-
-  // create the chart
-  const chart = Highcharts.stockChart("container", {
-    rangeSelector: {
-      selected: 1,
-    },
-
-    title: {
-      text: clickedCompanyName + "(" + clickedCompanyCode + ")"
-    },
-
-    series: [{
-      type: "candlestick",
-      name: clickedCompanyName + "(" + clickedCompanyCode + ")",
-      data: resultArr,
-      dataGrouping: {
-        units: [
-          [
-            "week", // unit name
-            [1], // allowed multiples
-          ],
-          ["month", [1, 2, 3, 4, 6]],
-        ],
-      },
-    },],
-    responsive: {
-      rules: [{
-        condition: {
-          maxWidth: 500,
-        },
-        chartOptions: {
-          chart: {
-            height: 205,
-          },
-          subtitle: {
-            text: null,
-          },
-          navigator: {
-            enabled: false,
-          },
-        },
-      },],
-    },
-  });
-
-  chart.setSize(null);
 }
